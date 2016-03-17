48c38bf14f6d3cf36968319972cecfdfc99bb9f2
Ludovic Roguet <ludovic.roguet@nordcloud.se>
2016-03-17 22:17:35 +0100

    Why bother with a variable for the current IP address?

--- scripts/cloudformation/update-route53-stack.sh
+++ scripts/cloudformation/update-route53-stack.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # To update https://github.com/lroguet/amazon-web-services/blob/master/cloudformation/templates/route53-dns-failover.json
 
-PRIMARY_RECORD_IP_ADDRESS="MY_IP_ADDRESS"
+PRIMARY_RECORD_IP_ADDRESS=`dig +short myip.opendns.com @resolver1.opendns.com`
 STACK_NAME_OR_ID="MY_STACK"
 
 aws cloudformation update-stack --stack-name $STACK_NAME_OR_ID \
